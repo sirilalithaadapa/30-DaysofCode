9f523b470ea1b994f0a3a2560d74dcc51be320c2
Siri Lalitha <sirilalitha9088@gmail.com>
2021-05-31 23:13:06 +0530

    Create day-11.py

---
+++ Day-11/day-11.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    maxx = -81 # if all the no are -9 then sum will be -81
+    for i in range(4):
+        for j in range(4):
+            total = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            if total > maxx:
+                maxx = total
+    print(maxx)
