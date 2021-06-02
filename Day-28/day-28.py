73797b6187eda6a128595ea6868aaf84f04e39d0
Siri Lalitha <sirilalitha9088@gmail.com>
2021-06-02 15:25:49 +0530

    Create day-28.py

---
+++ Day-28/day-28.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+    names = []
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+        firstName = firstNameEmailID[0]
+        emailID = firstNameEmailID[1]
+        if re.search(r"@gmail\.com$", emailID):
+            names.append(firstName)
+
+print(*sorted(names),sep='\n')
